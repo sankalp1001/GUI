46b1fcf07d25b5821770362a1ddfce7b8113af50
Sankalp Mallappa <75899963+sankalp1001@users.noreply.github.com>
2021-10-23 15:15:45 +0530

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -9,50 +9,40 @@ from PyQt5.QtCore import QThread, Qt, pyqtSignal,pyqtSlot
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QWidget, QLabel, QApplication
 import random
-from threading import *
-
+#from threading import Thread
+import threading
+    
 class vidFeed(QThread):
    ImgUpdate = pyqtSignal(QImage)
    def run(self):
         capture= cv2.VideoCapture(2)
         while True:
-           ret,frame =capture.read()
-           if ret:
-               Image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-               Flip = cv2.flip(Image,1)
-               QtFormat = QImage(Flip.data,Flip.shape[1],Flip.shape[0],QImage.Format_RGB888)
-               out =  QtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-               self.ImgUpdate.emit(out)
+            ret,frame =capture.read()
+            if ret:
+                Image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                Flip = cv2.flip(Image,1)
+                QtFormat = QImage(Flip.data,Flip.shape[1],Flip.shape[0],QImage.Format_RGB888)
+                out =  QtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.ImgUpdate.emit(out)
 
 
-class dispXY(QThread):
+
+class dispXY():         
     def genX(): 
-        while True: 
-            x = random.randint(1,1000)    
+        while True:
+            x = str(random.randint(1,1000)) 
+            #mainWindow.xcord.setText(x)  
             sleep(1)
             return str(x)
-
     def genY():
-        while True: 
-            y = random.randint(1,1000)    
+        while True:    
+            y = str(random.randint(1,1000)) 
+            # mainWindow.ycord.setText(y)   
             sleep(1)
-            return str(y)  
-    
-    def click_x(self):
-        while True:
-            #print("in x while")
-            y = self.genY()
-            self.xcord.setText(y)
-            sleep(1)
-    def click_y(self):
-        while True:
-            y = self.genX()
-            self.ycord.setText(y)
-            sleep(1)    
+            return str(y)
  
 class mainWindow(QWidget):
-   
-
+ 
     def uiSetup(self,Window):
         Window.setObjectName("MainWindow")
         Window.resize(800,640)
@@ -74,61 +64,50 @@ class mainWindow(QWidget):
         self.xcord.move(10,10)
         self.xcord.resize(280,40)
         self.xcord.setReadOnly(True)
-        #self.xcord.setText(str(dispXY.genY()))
+        # self.xcord.setText(str (random.randint(1,100)))
     
         self.ycord = QtWidgets.QTextEdit(self.centralwidget)
         self.ycord.move(10,51)
         self.ycord.resize(280,40)
         self.ycord.setReadOnly(True)
-        #self.ycord.setText(str(dispXY.genY()))
+        # self.ycord.setText(str(random.randint(1,100)))
 
         self.button=QtWidgets.QPushButton(self.centralwidget)
         self.button.setGeometry(QtCore.QRect(100, 400, 141, 41))
         self.button.clicked.connect(self.on_click)
         self.button.setText("Click")
-
-        '''self.xBut=QtWidgets.QPushButton(self.centralwidget)
-        self.xBut.setGeometry(QtCore.QRect(152, 250, 100, 21))
-        self.xBut.clicked.connect(self.click_x)
-        self.xBut.setText("y")
-
-        self.yBut=QtWidgets.QPushButton(self.centralwidget)
-        self.yBut.setGeometry(QtCore.QRect(10, 250, 100, 21))
-        self.yBut.clicked.connect(self.click_y)
-        self.yBut.setText("x")'''
-
+        
         Window.setCentralWidget(self.centralwidget)  
-        self.initUI(Window)
+        self.initUI()
         QtCore.QMetaObject.connectSlotsByName(Window)
-        self.setXY(dispXY.genX(),dispXY.genY())
-        #self.click_y()
-
-        #self.xyThre()
+        #self.setXY()
     def on_click(self):
         self.textbox.setText("Button Pressed")    
    
     @pyqtSlot(QImage)
     def setImage(self, image):
         self.CameraWindow.setPixmap(QPixmap.fromImage(image))
-    #def setXY(sef):
-    def setXY(self,x,y):
-        while True:
-            self.xcord.setText(x)
-            self.ycord.setText(y) 
-            sleep(1)
-    def initUI(self,mainWindow):
+    
+    def setXY(self):
+        if (dispXY.genX() or dispXY.genY):
+            self.xcord.setText(dispXY.genX())
+            self.ycord.setText(dispXY.genY())
+            #print(x,y)
+            #sleep(1)
+    def initUI(self):
         th =vidFeed(self)
         th.ImgUpdate.connect(self.setImage)
         th.start()
-        ''' th1 = dispXY(self)
-        th1.genX()
-        th1 = dispXY(self)
-        th1.start()'''
+        th1 = threading.Thread(target=self.setXY())
+        #th1.start()
+        
 
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     Window = QtWidgets.QMainWindow()
     ui = mainWindow()
     ui.uiSetup(Window)
     Window.show()
-    sys.exit(app.exec_())   
+    app.exec_()  
+    
